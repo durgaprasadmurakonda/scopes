fa805fb3425374261c5a54e349081d68aec05d98
durgaprasadmurakonda <durgaprasadmurakonda61@gmail.com>
2026-04-30 14:39:52 +0530

    send file into git hub

---
+++ lavanya.py
@@ -0,0 +1,11 @@
+
+
+import streamlit as st
+st.set_page_config(page_title="Wishes")
+st.title("🎓 TOPPER OF THE SCHOOL 🎓")
+
+st.write("Let's celebrate amazing success on hard work! Click below 🎉")
+if st.button("🎉 Click Here 🎉 "):
+    st.success("🎉 Congratulations Lavanya! 🎉")
+    st.balloons()
+    
